ed4e16adb3bb597c7c2df1c5a8aa177f652f7ff4
Tony Wilson <tony.wilson@hopenly.com>
2020-11-02 12:19:14 +0100

    Setting up  environment variables support.

---
+++ config.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+from decouple import config
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+KEYCLOAK_CLIENT_ID = config('KEYCLOAK_CLIENT_ID')
+KEYCLOAK_CLIENT_SECRET = config('KEYCLOAK_CLIENT_SECRET')
+KEYCLOAK_IDENTITY_OPENID_CONFIG_URL = config('KEYCLOAK_IDENTITY_OPENID_CONFIG_URL')
+
+MINIO_ENDPOINT = config('MINIO_ENDPOINT')
+
+MONGO_DATABASE_URL = config('MONGO_DATABASE_URL')
+MONGO_USER = config('MONGO_USER')
+MONGO_PASSWORD = config('MONGO_PASSWORD')
